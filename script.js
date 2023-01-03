b8af3b3a1f30ce22df35b28633948ca3fde19a53
MishraMuskan <mishramuskan014@gmail.com>
2023-01-03 20:34:07 +0530

    add some like features

--- script.js
+++ script.js
@@ -6,23 +6,16 @@ var arr = [{ dp: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?i
 { dp: "https://images.unsplash.com/photo-1517805686688-47dd930554b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTYxfHxtb2RlbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60", story: "https://images.unsplash.com/photo-1496440737103-cd596325d314?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTM2fHxtb2RlbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60", name: "anbhavi", },
 { dp: "https://images.unsplash.com/photo-1615212123996-f21711429486?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTY3fHxtb2RlbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60", story: "https://images.unsplash.com/photo-1616501470925-5aa85c62679e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTQxfHxtb2RlbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60", name: "ismita_😍", },
 ]
-
 var clutter = "";
 arr.forEach(function (elem, index) {
     clutter += ` <div id="story1" class="a">
     <div id="image0" class="imagee">
  <img id = "${index}" src="${elem.dp}"alt=""></img>
-   
     </div>
      <h4>${elem.name}</h4>
-    </div>
-   
-    `
+    </div>`
 })
-
-
 document.querySelector("#story").innerHTML = clutter;
-
 var timer = 0;
 document.querySelector("#story").addEventListener("click", function (dets) {
     document.querySelector("#fs").style.display = "initial";
@@ -44,7 +37,6 @@ var array = [{ profiles: "https://images.unsplash.com/photo-1529626455594-4ff080
 { profiles: "https://images.unsplash.com/photo-1541182388496-ac92a3230e4c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTM4fHxtb2RlbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60", posts: "https://images.unsplash.com/photo-1604537372136-89b3dae196e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxzZWFyY2h8MjJ8fHNub3d8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60", name: "gk_facts" },
 { profiles: "https://images.unsplash.com/photo-1517805686688-47dd930554b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTYxfHxtb2RlbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60", posts: "https://images.unsplash.com/photo-1670100053465-aacb32bf96ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDc3fEpwZzZLaWRsLUhrfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", name: "aispirants" },
 { profiles: "https://images.unsplash.com/photo-1615212123996-f21711429486?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTY3fHxtb2RlbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60", posts: "https://images.unsplash.com/photo-1603029084766-3ddc1c5c7e94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTF8fGlhcyUyMHF1b3Rlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60", name: "lbsnaaquotes" },]
-
 var clutter1 = "";
 array.forEach(function (val, idx) {
     clutter1 += ` 
@@ -78,33 +70,24 @@ array.forEach(function (val, idx) {
  </div>
  </div> `
 })
-
 document.querySelector("#box").innerHTML = clutter1;
-
 var instalike = document.querySelectorAll(".post");
 var postLike = document.querySelectorAll('.post-like');
-
-
-
 instalike.forEach((elem) => {
     elem.addEventListener('dblclick', function (dets) {
         dets.path[2].children[2].children[0].children[0].classList.remove("ri-heart-line")
         dets.path[2].children[2].children[0].children[0].classList.add("ri-heart-fill")
         dets.path[2].children[2].children[0].children[0].style.color = "red";
-
-
                dets.target.parentElement.children[0].style.display = "initial";
         setTimeout(function () {
             dets.target.parentElement.children[0].style.display = "none";
         }, 1000)
-
-
     })
 })
 // var flag = 0;
 // var stry = document.querySelector("#story")
-// stry.forEach(function(elem){
-//        if(flag==0){
-//         document.querySelector("#story").in
-//        }
-// })
+// for (let flag = 0; flag < Infinity; flag++) {
+   
+
+//     // document.querySelector("#fs").style = "infinite"
+//   }
